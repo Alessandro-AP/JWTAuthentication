8651675b611c7fa5542ab51a472e33606185cc20
Alessandro-AP <alessandro.parrinoap@gmail.com>
2022-01-05 17:04:17 +0100

    prepare for deploy

--- src/main/java/com/example/springjwt/SpringJwtApplication.java
+++ src/main/java/com/example/springjwt/SpringJwtApplication.java
@@ -1,16 +1,10 @@
 package com.example.springjwt;
 
-import com.example.springjwt.model.Role;
-import com.example.springjwt.model.User;
-import com.example.springjwt.service.UserService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.ArrayList;
 
 @SpringBootApplication
 public class SpringJwtApplication {
@@ -27,29 +21,5 @@ public class SpringJwtApplication {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    /**
-     * Function executing at the launch of the application,
-     * inserts some basic data in the mysql database
-     */
-    @Bean
-    CommandLineRunner run(UserService userService){
-        return args -> {
-          //If the database is not empty, default data is not added
-          if(userService.getUsers() != null) {
-              userService.saveRole(new Role(null, "user"));
-              userService.saveRole(new Role(null, "admin"));
-
-              userService.saveUser(new User(null, "admin", "Xkxtxyglu@z9Afacb-k1", new ArrayList<>()));
-              userService.saveUser(new User(null, "alessandro", "Amtech21.", new ArrayList<>()));
-              userService.saveUser(new User(null, "daniel", "Amtech21.", new ArrayList<>()));
-              userService.saveUser(new User(null, "gaetan", "Amtech21.", new ArrayList<>()));
-              userService.saveUser(new User(null, "anh", "Amtech21.", new ArrayList<>()));
-              userService.saveUser(new User(null, "markou", "Amtech21.", new ArrayList<>()));
-
-              userService.addRoleToUser("admin", "admin");
-          }
-        };
     }
 }
